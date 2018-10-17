5762a27b5962b31075d3b7902d7dbe48a31cf6eb
Adam Zsarnoczay <33822153+zsarnoczay@users.noreply.github.com>
2018-10-17 15:30:25 -0700

    remove past import to avoid issues with older python versions

--- applications/Workflow/PBE.py
+++ applications/Workflow/PBE.py
@@ -5,8 +5,6 @@ from __future__ import division, print_function
 import sys
 if sys.version.startswith('2'): 
     range=xrange
-else:
-    from past.builtins import basestring
 
 import json
 import os
