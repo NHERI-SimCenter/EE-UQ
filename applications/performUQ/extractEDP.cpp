160c20d342eda3e36e8d4c177d7a26ce01cdce02
fmckenna <fmckenna@berkeley.edu>
2019-05-22 16:01:24 -0700

    fmk - changes for result files to be copied

--- applications/performUQ/extractEDP.cpp
+++ applications/performUQ/extractEDP.cpp
@@ -8,6 +8,9 @@
 //using namespace std
 
 #include <jansson.h>  // for Json
+#ifndef _WIN32
+#include <sys/stat.h>
+#endif
 
 int main(int argc, const char **argv)
 {
@@ -52,11 +55,72 @@ int main(int argc, const char **argv)
 	}
       }
     }
-
   }
+
   printf("NUM_EDP= %d\n",numEDP);
 
   fOut.close();
+
+  //
+  // process result files
+
+  if (argc > 5) {
+    const char *dakotaIN = argv[3];
+    const char *numRF = argv[4];
+    int numFiles = atoi(numRF);
+
+    std::cerr << "numFILES: " << numFiles << "\n";
+
+    if (numFiles >= 1) {
+      // mkdir
+      char copyFile[1000]; 
+#if defined(_WIN32)
+      std::string dirName("..\\Results\\");
+      _mkdir("..\\Results");
+#else
+      std::string dirName("../Results/");
+      mkdir("../Results", 0777);
+#endif
+
+      int evalID = -1;
+      json_error_t error;
+      json_t *rootDakota = json_load_file(dakotaIN, 0, &error);
+      json_t *uqMethod = json_object_get(rootDakota,"UQ_Method");  
+      if (uqMethod != NULL) {
+	std::cerr << "HELLO1";
+	json_t *samplingData = json_object_get(uqMethod,"samplingMethodData");
+	if (samplingData != NULL) {
+	std::cerr << "HELLO2";
+	  json_t *evalID_Data = json_object_get(samplingData,"evalID");
+	  if (evalID_Data != NULL) {
+	    std::cerr << "HELLO3";
+	    evalID = json_integer_value(evalID_Data);
+	  }
+	}
+      }
+      
+      if (evalID != -1) {
+	for (int i=0; i<numFiles; i++) {
+	  const char *filename = argv[5+i];
+	  std::string name(filename);
+	  std::string copy = dirName + std::to_string(evalID) + std::string("-") + name;
+	  std::cerr << copy << "\n";
+
+	  std::ifstream source(filename, std::ios::binary);
+	  std::ofstream dest(copy, std::ios::binary);
+
+	  std::istreambuf_iterator<char> begin_source(source);
+	  std::istreambuf_iterator<char> end_source;
+	  std::ostreambuf_iterator<char> begin_dest(dest); 
+	  std::copy(begin_source, end_source, begin_dest);
+
+	  source.close();
+	  dest.close();
+
+	}
+      }   
+    }
+  }
 
   return 0;
 }
