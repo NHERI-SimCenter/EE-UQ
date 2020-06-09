91b00bd8c21302ae6d5df8e88d32665c3f40eb1e
fmckenna <fmckenna@berkeley.edu>
2020-06-08 18:48:54 -0700

    fmk - forgot to save to files in recorders

--- Examples/ShearBuilding3/recorderCommands.tcl
+++ Examples/ShearBuilding3/recorderCommands.tcl
@@ -1,3 +1,3 @@
 
-recorder EnvelopeNode -node 1 2 3 4 -dof 1 2 disp
-recorder EnvelopeElement -ele 1 2 3 forces
+recorder EnvelopeNode -file node.out -node 1 2 3 4 -dof 1 2 disp
+recorder EnvelopeElement -file forces.out -ele 1 2 3 forces
