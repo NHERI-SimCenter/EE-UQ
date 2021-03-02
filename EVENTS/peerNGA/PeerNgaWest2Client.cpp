c7e5830be086cef179294e6c7bad08c6b4b532a0
fmckenna <fmckenna@berkeley.edu>
2021-03-02 10:22:13 -0800

    fmk - fixing search criteria related to SRkey

--- EVENTS/peerNGA/PeerNgaWest2Client.cpp
+++ EVENTS/peerNGA/PeerNgaWest2Client.cpp
@@ -83,15 +83,17 @@ void PeerNgaWest2Client::selectRecords(QList<QPair<double, double>> spectrum, in
     commitPart.setBody("Upload");
     multiPart->append(commitPart);
 
+    recordsToDownload.clear();
     uploadFileReply = networkManager.post(uploadFileRequest, multiPart);
 }
 
-void PeerNgaWest2Client::selectRecords(double sds, double sd1, double tl, int nRecords, QVariant magnitudeRange, QVariant distanceRange, QVariant vs30Range)
+void PeerNgaWest2Client::selectRecords(double sds, double sd1, double tl, int nRecords, QVariant magnitudeRange, QVariant distanceRange, QVariant vs30Range, int peerSRkey)
 {
     emit selectionStarted();
     emit statusUpdated("Performing Record Selection...");
 
     this->nRecords = nRecords;
+    this->SRkey = peerSRkey;
 
     QNetworkCookie cookie("SpectrumModel_Dropdown", "99");
     cookie.setDomain("ngawest2.berkeley.edu");
@@ -117,6 +119,33 @@ void PeerNgaWest2Client::selectRecords(double sds, double sd1, double tl, int nR
     this->distanceRange = distanceRange;
     this->vs30Range = vs30Range;
 
+    recordsToDownload.clear();
+    postSpectraReply = networkManager.post(postSpectraRequest, postSpectraParameters.query().toUtf8());
+}
+
+void PeerNgaWest2Client::selectRecords(QStringList records)
+{
+    emit selectionStarted();
+    emit statusUpdated("Performing Record Selection...");
+
+    QNetworkCookie cookie("SpectrumModel_Dropdown", "88");
+    cookie.setDomain("ngawest2.berkeley.edu");
+    networkManager.cookieJar()->insertCookie(cookie);
+
+    postSpectraRequest.setUrl(QUrl("https://ngawest2.berkeley.edu/spectras"));
+    postSpectraRequest.setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
+
+    postSpectraParameters.clear();
+    postSpectraParameters.addQueryItem("authenticity_token", authenticityToken);
+    postSpectraParameters.addQueryItem("spectra[NGAInputData_NGAModelSelection]", "88");//88 no spectrum
+    postSpectraParameters.addQueryItem("model[AS]", "0");
+    postSpectraParameters.addQueryItem("model[BA]", "0");
+    postSpectraParameters.addQueryItem("model[CY]", "0");
+    postSpectraParameters.addQueryItem("model[CB]", "0");
+    postSpectraParameters.addQueryItem("model[ID]", "0");
+    postSpectraParameters.addQueryItem("spectra[menu_Mechanism]", "1");
+
+    recordsToDownload = records;
     postSpectraReply = networkManager.post(postSpectraRequest, postSpectraParameters.query().toUtf8());
 }
 
@@ -138,7 +167,7 @@ void PeerNgaWest2Client::processNetworkReply(QNetworkReply *reply)
        qDebug() << QSslSocket::sslLibraryBuildVersionString();
 
        qDebug() << "support ssl: " << QSslSocket::supportsSsl();
-*/
+  */
     if(reply == signInPageReply)
         processSignInPageReply();
 
@@ -214,6 +243,17 @@ void PeerNgaWest2Client::processSignInPageReply()
     signInReply = networkManager.post(peerSignInRequest, signInParameters.query().toUtf8());
 }
 
+
+static void debugRequest(QNetworkRequest request, QByteArray data = QByteArray())
+{
+  qDebug() << request.url().toString();
+  const QList<QByteArray>& rawHeaderList(request.rawHeaderList());
+  foreach (QByteArray rawHeader, rawHeaderList) {
+    qDebug() << request.rawHeader(rawHeader);
+  }
+  qDebug() << data;
+}
+
 void PeerNgaWest2Client::processSignInReply()
 {
     if(signInReply->error() == QNetworkReply::NoError)
@@ -248,6 +288,8 @@ void PeerNgaWest2Client::processSignInReply()
 
 void PeerNgaWest2Client::processUploadFileReply()
 {
+     qDebug() << "PROCESS UPLOAD FILE REPLY";
+
     QNetworkCookie cookie("SpectrumModel_Dropdown", "0");
     cookie.setDomain("ngawest2.berkeley.edu");
     networkManager.cookieJar()->insertCookie(cookie);
@@ -270,12 +312,15 @@ void PeerNgaWest2Client::processUploadFileReply()
             postSpectraParameters.addQueryItem("spectra[filename]", cookie.value());
 
 
+    //debugRequest(postSpectraRequest, postSpectraParameters.query().toUtf8());
     postSpectraReply = networkManager.post(postSpectraRequest, postSpectraParameters.query().toUtf8());
 }
 
 void PeerNgaWest2Client::processPostSpectrumReply()
 {
-    qDebug() << postSpectraReply->readAll();
+    qDebug() << "PROCESS POST SPECTRUIM REPLY";
+
+    QByteArray data = postSpectraReply->readAll();
     if(postSpectraReply->error() != QNetworkReply::NoError)
     {
         //if it fails we will retry
@@ -298,77 +343,120 @@ void PeerNgaWest2Client::processPostSpectrumReply()
 
         retries = 0;
         auto url = postSpectraReply->attribute(QNetworkRequest::RedirectionTargetAttribute).toUrl();
-        auto searchPost = url.toString().remove("/new").remove("/edit").append("/searches");
+        auto searchPost = url.toString().remove("/new").remove("/edit").remove("/searches").append("/searches");
 
         QNetworkRequest searchPostRequest(searchPost);
         searchPostRequest.setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
 
         QUrlQuery params;
-
         params.addQueryItem("authenticity_token", authenticityToken);
-        params.addQueryItem("search[search_nga_number]", "");
         params.addQueryItem("search[DampingRatio]", "0.05");
-        params.addQueryItem("search[SRkey]", "1");
+        params.addQueryItem("search[SRkey]", QString::number(this->SRkey));
         params.addQueryItem("search[search_station_name]", "");
         params.addQueryItem("search[search_eq_name]", "");
 
-        if(searchScaleFlag == 0)
-        {
+        qDebug() << "RECORDS TO DOWNLOAD: " << recordsToDownload;
+
+        if (recordsToDownload.length() == 0) {
+
+            // it is  a search, provide selection quantaties
+            params.addQueryItem("search[search_nga_number]", "");
+
+
+            if(searchScaleFlag == 0)
+            {
+                params.addQueryItem("search[scale_flag]", "0");
+            }
+            else if(searchScaleFlag == 1)
+            {
+                params.addQueryItem("search[scale_flag]", "1");
+                params.addQueryItem("search[period]", searchPeriodPoints);
+                params.addQueryItem("search[weight]", searchWeightPoints);
+            }
+            else if(searchScaleFlag == 2)
+            {
+                params.addQueryItem("search[scale_flag]", "2");
+                params.addQueryItem("search[SinglePeriodScalingT]", searchSinglePeriodScalingT);
+                params.addQueryItem("search[period]", searchPeriodPoints);
+                params.addQueryItem("search[weight]", searchWeightPoints);
+            }
+
+            params.addQueryItem("search[output_num]", QString::number(nRecords));
+
+            if(magnitudeRange.isValid() && !magnitudeRange.isNull())
+            {
+                auto mangnitudePair = magnitudeRange.value<QPair<double, double>>();
+                QString magnitude = QString::number(mangnitudePair.first) + ',' + QString::number(mangnitudePair.second);
+                params.addQueryItem("search[magnitude]", magnitude);
+            }
+
+            if(distanceRange.isValid() && !distanceRange.isNull())
+            {
+                auto distancePair = distanceRange.value<QPair<double, double>>();
+                QString rrup = QString::number(distancePair.first) + ',' + QString::number(distancePair.second);
+                params.addQueryItem("search[rrup]", rrup);
+            }
+
+            if(vs30Range.isValid() && !vs30Range.isNull())
+            {
+                auto vs30Pair = vs30Range.value<QPair<double, double>>();
+                QString vs30 = QString::number(vs30Pair.first) + ',' + QString::number(vs30Pair.second);
+                params.addQueryItem("search[vs30]", vs30);
+            }
+        } else {
+
+            // just want to download specific records
+            QString list;
+            int lengthRecords = recordsToDownload.length();
+            for (int i=0; i<lengthRecords; i++) {
+                list.append(recordsToDownload.at(i));
+                if (i != lengthRecords-1)
+                    list.append(",");
+            }
+
+            qDebug() << "Peer List: " << list;
+
+            params.addQueryItem("search[search_nga_number]", list);
             params.addQueryItem("search[scale_flag]", "0");
-        }
-        else if(searchScaleFlag == 1)
-        {
-            params.addQueryItem("search[scale_flag]", "1");
-            params.addQueryItem("search[period]", searchPeriodPoints);
-            params.addQueryItem("search[weight]", searchWeightPoints);
-        }
-        else if(searchScaleFlag == 2)
-        {
-            params.addQueryItem("search[scale_flag]", "2");
-            params.addQueryItem("search[SinglePeriodScalingT]", searchSinglePeriodScalingT);
-            params.addQueryItem("search[period]", searchPeriodPoints);
-            params.addQueryItem("search[weight]", searchWeightPoints);
-        }
-
-        params.addQueryItem("search[output_num]", QString::number(nRecords));
-
-        if(magnitudeRange.isValid() && !magnitudeRange.isNull())
-        {
-            auto mangnitudePair = magnitudeRange.value<QPair<double, double>>();
-            QString magnitude = QString::number(mangnitudePair.first) + ',' + QString::number(mangnitudePair.second);
-            params.addQueryItem("search[magnitude]", magnitude);
+            params.addQueryItem("search[faultType]", "1");
+            params.addQueryItem("search[magnitude]", "");
+            params.addQueryItem("search[rjb]", "");
+            params.addQueryItem("search[rrup]", "");
+            params.addQueryItem("search[vs30]", "");
+            params.addQueryItem("search[duration]", "");
+            params.addQueryItem("search[pulse]", "1");
+            params.addQueryItem("search[Pulse]", "1");
+            params.addQueryItem("search[synth_FaultType]", "0");
+            params.addQueryItem("search[synth_Adjacency]", "0");
+            params.addQueryItem("search[SRmeanFlag]", "0");
+            params.addQueryItem("search[vs30]", "");
+            params.addQueryItem("search[rrup]", "");
+            params.addQueryItem("search[magnitude]", "");
         }
 
-        if(distanceRange.isValid() && !distanceRange.isNull())
-        {
-            auto distancePair = distanceRange.value<QPair<double, double>>();
-            QString rrup = QString::number(distancePair.first) + ',' + QString::number(distancePair.second);
-            params.addQueryItem("search[rrup]", rrup);
-        }
-
-        if(vs30Range.isValid() && !vs30Range.isNull())
-        {
-            auto vs30Pair = vs30Range.value<QPair<double, double>>();
-            QString vs30 = QString::number(vs30Pair.first) + ',' + QString::number(vs30Pair.second);
-            params.addQueryItem("search[vs30]", vs30);
-        }
-
+        debugRequest(searchPostRequest, params.query().toUtf8());
         postSearchReply = networkManager.post(searchPostRequest, params.query().toUtf8());
     }
 }
 
 void PeerNgaWest2Client::processPostSearchReply()
 {
+     qDebug() << "PROCESS POST SEARCH REPLY";
     emit statusUpdated("Retrieving Record Selection Results from PEER NGA West 2 Database");
 
     auto url = postSearchReply->attribute(QNetworkRequest::RedirectionTargetAttribute).toUrl();
     auto getRecordsUrl = url.toString().replace("/edit", "/?getRecords=1");
+
+
+
     QNetworkRequest getRecordsRequest(getRecordsUrl);
+    qDebug() << "URL: " << getRecordsRequest.url().toString();
     getRecordsReply = networkManager.get(getRecordsRequest);
 }
 
 void PeerNgaWest2Client::processGetRecordsReply()
 {
+
     emit statusUpdated("Downloading Ground Motions from PEER NGA West 2 Database");
     auto replyText = QString(getRecordsReply->readAll());
     auto url = replyText.remove("window.location.href = \"").remove("\";").prepend("https://ngawest2.berkeley.edu");
@@ -380,6 +468,7 @@ void PeerNgaWest2Client::processGetRecordsReply()
 
 void PeerNgaWest2Client::processDownloadRecordsReply()
 {
+     qDebug() << "PROCESS DOWNLOADS RECORD REPLY";
     emit selectionFinished();
     auto tempLocation = QStandardPaths::writableLocation(QStandardPaths::TempLocation);
 
@@ -392,14 +481,19 @@ void PeerNgaWest2Client::processDownloadRecordsReply()
     }
     //TODO: we might need to use temporary files
     QString recordsPath = tempLocation.append("/PeerRecords.zip");
+
+    qDebug() << "RECORDS PATH: " << recordsPath;
+
     QFile file(recordsPath);
     if(!file.open(QIODevice::WriteOnly))
     {
         emit statusUpdated("Ground Motions Download Failed!");
         return;
     }
+
     file.write(downloadRecordsReply->readAll());
     file.close();
+
     emit statusUpdated("Ground Motions Downloaded Sucessfully");
     emit recordsDownloaded(recordsPath);
 }
