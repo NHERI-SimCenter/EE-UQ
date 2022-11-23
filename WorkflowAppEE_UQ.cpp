0decf728dfa3fe2834e68c45249dd104cbb9b373
bsaakash <11618528+bsaakash@users.noreply.github.com>
2022-11-22 18:41:03 -0800

    abs - fixing check for FEM method data

--- WorkflowAppEE_UQ.cpp
+++ WorkflowAppEE_UQ.cpp
@@ -395,7 +395,7 @@ WorkflowAppEE_UQ::inputFromJSON(QJsonObject &jsonObject)
 
         if (theSIM->inputAppDataFromJSON(theApplicationObject) == false)
             this->errorMessage("EE_UQ: failed to read SIM application");
-	
+    
         if (theAnalysisSelection->inputAppDataFromJSON(theApplicationObject) == false)
             this->errorMessage("EE_UQ: failed to read FEM application");
 
@@ -434,8 +434,6 @@ WorkflowAppEE_UQ::inputFromJSON(QJsonObject &jsonObject)
        this->errorMessage("EE_UQ: failed to read UQ Method data");
 
     if (theAnalysisSelection->inputFromJSON(jsonObject) == false)
-
-    if (theSIM->inputFromJSON(jsonObject) == false)
         this->errorMessage("EE_UQ: failed to read FEM Method data");
 
     if (theSIM->inputFromJSON(jsonObject) == false)
