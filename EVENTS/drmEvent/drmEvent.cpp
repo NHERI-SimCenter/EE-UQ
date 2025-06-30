fd3cb9497f98e9a64f26315a8c0ddb117639ce25
ampk <amnp95@uw.edu>
2025-06-30 16:07:13 -0700

    Add DRM Event functionality with local event widget integration

---
+++ EVENTS/drmEvent/drmEvent.cpp
@@ -0,0 +1,78 @@
+#include "drmEvent.h"
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QJsonObject>
+#include <QJsonArray>
+
+DRMevent::DRMevent(QWidget *parent) : SimCenterAppWidget(parent) {
+    eventTypeCombo = new QComboBox(this);
+    eventTypeCombo->addItem("Local");
+    eventTypeCombo->addItem("Predefined-DesignSafe");
+    eventTypeCombo->addItem("Convolution");
+    eventTypeCombo->addItem("Remote-DesignSafe");
+
+    stackedWidget = new QStackedWidget(this);
+    localWidget = new DRMLocalEventWidget(this);
+    predefWidget = new QLabel("Predefined-DesignSafe event selection not implemented yet.", this);
+    convWidget = new QLabel("Convolution event selection not implemented yet.", this);
+    remoteWidget = new QLabel("Remote-DesignSafe event selection not implemented yet.", this);
+
+    stackedWidget->addWidget(localWidget);
+    stackedWidget->addWidget(predefWidget);
+    stackedWidget->addWidget(convWidget);
+    stackedWidget->addWidget(remoteWidget);
+
+    QVBoxLayout *mainLayout = new QVBoxLayout(this);
+    mainLayout->addWidget(new QLabel("DRM Event Type:"));
+    mainLayout->addWidget(eventTypeCombo);
+    mainLayout->addWidget(stackedWidget);
+    setLayout(mainLayout);
+
+    connect(eventTypeCombo, QOverload<int>::of(&QComboBox::currentIndexChanged),
+            stackedWidget, &QStackedWidget::setCurrentIndex);
+}
+
+bool DRMevent::outputToJSON(QJsonObject &jsonObject) {
+    // Only output the currently selected event
+    int idx = eventTypeCombo->currentIndex();
+    if (idx == 0) {
+        return localWidget->outputToJSON(jsonObject);
+    }
+    return true;
+}
+
+bool DRMevent::inputFromJSON(QJsonObject &jsonObject) {
+    QStringList keys = jsonObject.keys();
+    for (const QString &key : keys) {
+    }
+    if (jsonObject.contains("type") && jsonObject["type"].toString() == "DRM") {
+        if (jsonObject.contains("system") && jsonObject["system"].toString() == "local") {
+            eventTypeCombo->setCurrentIndex(0); // Default to local if not specified
+            return localWidget->inputFromJSON(jsonObject);
+        }
+    }
+    // print key names for debugging
+    return true;
+}
+
+bool DRMevent::outputAppDataToJSON(QJsonObject &jsonObject) {
+    int idx = eventTypeCombo->currentIndex();
+    if (idx == 0) {
+        return localWidget->outputAppDataToJSON(jsonObject);
+    }
+    return true;
+}
+
+bool DRMevent::inputAppDataFromJSON(QJsonObject &jsonObject) {
+    return true;
+}
+
+bool DRMevent::copyFiles(QString &destDir) {
+    // Implement as needed
+    return true;
+}
+
+void DRMevent::clear() {
+    localWidget->clear();
+    eventTypeCombo->setCurrentIndex(0);
+}
---
+++ EVENTS/drmEvent/drmEvent.h
@@ -0,0 +1,31 @@
+#ifndef DRMEVENT_H
+#define DRMEVENT_H
+
+#include <SimCenterAppWidget.h>
+#include <QComboBox>
+#include <QStackedWidget>
+#include <QVBoxLayout>
+#include <QLabel>
+#include "drmLocalEventWidget.h"
+
+class DRMevent : public SimCenterAppWidget {
+    Q_OBJECT
+public:
+    explicit DRMevent(QWidget *parent = nullptr);
+    bool outputToJSON(QJsonObject &jsonObject) override;
+    bool inputFromJSON(QJsonObject &jsonObject) override;
+    bool outputAppDataToJSON(QJsonObject &jsonObject) override;
+    bool inputAppDataFromJSON(QJsonObject &jsonObject) override;
+    bool copyFiles(QString &destDir) override;
+    void clear() override;
+
+private:
+    QComboBox *eventTypeCombo;
+    QStackedWidget *stackedWidget;
+    DRMLocalEventWidget *localWidget;
+    QWidget *predefWidget;
+    QWidget *convWidget;
+    QWidget *remoteWidget;
+};
+
+#endif // DRMEVENT_H
